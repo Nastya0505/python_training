a858cd71c13ef3cbb0f4b92bb757455e4fa8347c
Nastya0505 <100522937+Nastya0505@users.noreply.github.com>
2022-11-12 16:08:43 +0300

    гитигнор

--- tests/tests_delete.py
+++
@@ -1,7 +0,0 @@
-def test_delete_first_group(app):
-    app.group.delete_first_group()
-
-def test_delete_first_contact(app):
-    app.contact.delete_first_contact()
-
-
--- tests/tests_modify.py
+++
@@ -1,6 +0,0 @@
-def test_modify_first_group(app):
-    app.group.modify_first_group()
-
-def test_modify_first_contact(app):
-    app.contact.modify_first_contact()
-
